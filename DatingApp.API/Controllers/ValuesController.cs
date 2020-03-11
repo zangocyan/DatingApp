9c5c5f5cd3d45395509153bb3d7db07406abc0c4
Murat Tekin <muratbilaltekin@gmail.com>
2020-03-11 21:27:53 +0300

    End of section 4

--- DatingApp.API/Controllers/ValuesController.cs
+++ DatingApp.API/Controllers/ValuesController.cs
@@ -21,6 +21,7 @@ namespace DatingApp.API.Controllers
 
         }
         // GET api/values
+        [AllowAnonymous]
         [HttpGet]
         public async Task<IActionResult> GetValues()
         {
@@ -29,7 +30,7 @@ namespace DatingApp.API.Controllers
         }
 
         // GET api/values/5
-        [AllowAnonymous]
+        [AllowAnonymous]    
         [HttpGet("{id}")]
         public async Task<IActionResult> GetValue(int id)
         {
